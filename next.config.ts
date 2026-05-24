d961dce02f8a7ef3ccc4354d1e1978145f1f7f8a
David Odinga <divadagnido96@gmail.com>
2026-05-24 22:50:30 +0300

    Update workflow

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+    output: 'export',
+    basePath: '/vektaedge'
+};
 
 export default nextConfig;
